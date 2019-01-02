4a9e707892c0808a44b099d64b426cf5a844db84
Samuel Pickard <sjp@appmancer.com>
2019-01-02 15:55:53 +0000

    Fixed adding new player.  Started work on MicroPython player/client

--- TestPublisher.py
+++ TestPublisher.py
@@ -4,5 +4,5 @@ import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
 client.connect("localhost",1883,60)
-client.publish("SuperQuiz/player_registrar", "{\"Player\":\"Cow\"}");
+client.publish("SuperQuiz/player_registrar", "{\"Player\":\"cow\"}");
 client.disconnect();
--- TkSuperQuiz.py
+++ TkSuperQuiz.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 import paho.mqtt.client as mqtt
 import json
+from tkinter import *
 
 #Style constants
 bgColor = "#F7FFF7"
 titleTextColor = "#1a535c"
 
-#text constants
+#text constants5
 cat         = "cat"
 cockerel    = "cockerel"
 cow         = "cow"
@@ -21,31 +22,36 @@ pig         = "pig"
 woodpecker  = "woodpecker"
 
 animals = [cat, cockerel, cow, dog, elephant, goose, horse, lion, monkey, mouse, pig, woodpecker]
-normalImages = [ cat        : tk.PhotoImage(file="Normal/cat.png"),
-                 cockerel   : tk.PhotoImage(file="Normal/cockerel.png"),
-                 cow        : tk.PhotoImage(file="Normal/cow.png"),
-                 dog        : tk.PhotoImage(file="Normal/dog.png"),
-                 elephant   : tk.PhotoImage(file="Normal/elephant.png"),
-                 goose      : tk.PhotoImage(file="Normal/goose.png"),
-                 horse      : tk.PhotoImage(file="Normal/horse.png"),
-                 lion       : tk.PhotoImage(file="Normal/lion.png"),
-                 monkey     : tk.PhotoImage(file="Normal/monkey.png"),
-                 mouse      : tk.PhotoImage(file="Normal/mouse.png"),
-                 pig        : tk.PhotoImage(file="Normal/pig.png"),
-                 woodpecker : tk.PhotoImage(file="Normal/woodpecker.png")]
 
-selectedImages = [ cat        : tk.PhotoImage(file="Selected/cat.png"),
-                 cockerel   : tk.PhotoImage(file="Selected/cockerel.png"),
-                 cow        : tk.PhotoImage(file="Selected/cow.png"),
-                 dog        : tk.PhotoImage(file="Selected/dog.png"),
-                 elephant   : tk.PhotoImage(file="Selected/elephant.png"),
-                 goose      : tk.PhotoImage(file="Selected/goose.png"),
-                 horse      : tk.PhotoImage(file="Selected/horse.png"),
-                 lion       : tk.PhotoImage(file="Selected/lion.png"),
-                 monkey     : tk.PhotoImage(file="Selected/monkey.png"),
-                 mouse      : tk.PhotoImage(file="Selected/mouse.png"),
-                 pig        : tk.PhotoImage(file="Selected/pig.png"),
-                 woodpecker : tk.PhotoImage(file="Selected/woodpecker.png")]
+class ContestantImages:
+    def __init__(self, parent, *args, **kwargs):
+        self.normalImages = { cat        : tk.PhotoImage(file="Normal/cat.png"),
+                         cockerel   : tk.PhotoImage(file="Normal/cockerel.png"),
+                         cow        : tk.PhotoImage(file="Normal/cow.png"),
+                         dog        : tk.PhotoImage(file="Normal/dog.png"),
+                         elephant   : tk.PhotoImage(file="Normal/elephant.png"),
+                         goose      : tk.PhotoImage(file="Normal/goose.png"),
+                         horse      : tk.PhotoImage(file="Normal/horse.png"),
+                         lion       : tk.PhotoImage(file="Normal/lion.png"),
+                         monkey     : tk.PhotoImage(file="Normal/monkey.png"),
+                         mouse      : tk.PhotoImage(file="Normal/mouse.png"),
+                         pig        : tk.PhotoImage(file="Normal/pig.png"),
+                         woodpecker : tk.PhotoImage(file="Normal/woodpecker.png")}
+
+        self.selectedImages = { cat      : tk.PhotoImage(file="Selected/cat.png"),
+                         cockerel   : tk.PhotoImage(file="Selected/cockerel.png"),
+                         cow        : tk.PhotoImage(file="Selected/cow.png"),
+                         dog        : tk.PhotoImage(file="Selected/dog.png"),
+                         elephant   : tk.PhotoImage(file="Selected/elephant.png"),
+                         goose      : tk.PhotoImage(file="Selected/goose.png"),
+                         horse      : tk.PhotoImage(file="Selected/horse.png"),
+                         lion       : tk.PhotoImage(file="Selected/lion.png"),
+                         monkey     : tk.PhotoImage(file="Selected/monkey.png"),
+                         mouse      : tk.PhotoImage(file="Selected/mouse.png"),
+                         pig        : tk.PhotoImage(file="Selected/pig.png"),
+                         woodpecker : tk.PhotoImage(file="Selected/woodpecker.png")}
+        
+
 
 class QuizPlayer(tk.Label):
     def __init__(self, parent, *args, **kwargs):
@@ -60,16 +66,23 @@ class ReadyPlayerList(tk.Frame):
         self.title.pack(side="top", anchor="n", fill="x", expand=True)
         self.activePlayers = []
 
-    def reset:
+    def reset(self):
         self.activePlayers = []
 
-    def addPlayer(self, playerName):
+    def addPlayer(self, playerName, playerImages):
+        print("Adding new player image " + playerName)
         if self.activePlayers.count(playerName) == 0:
+            print(playerName + " is not already in the list")
             if animals.count(playerName) > 0:
+                print("Adding player '" + playerName + "'")
+                newPlayer = QuizPlayer(self, text=playerName, image=playerImages.normalImages[playerName])
+                # newPlayer = QuizPlayer(self, text=playerName)
+                newPlayer.pack()
                 self.activePlayers.append(playerName)
-                if playerName == cat:
-                    addNewPlayer(cat, self.cat)
-            
+            else:
+                print("Error: Animal '" + playerName + "' not found.")
+        else:
+            print("Warning: Animal '" + playerName + "' is already in list")
         
 class BuzzedInPlayerList(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -81,6 +94,7 @@ class SuperQuiz(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.images = ContestantImages(self)
         self.contestants = ReadyPlayerList(self, bg=bgColor)
         self.contestants.pack(side="left", anchor="n", fill="x", expand=False)
         self.answers = BuzzedInPlayerList(self, bg=bgColor)
@@ -89,10 +103,20 @@ class SuperQuiz(tk.Frame):
 
     def newPlayer(self, msg):
         #adding a new player to the list
+        print("Adding new player " + msg)
+        self.contestants.addPlayer(msg, playerImages=self.images)
+        #add the image to the active contestants
+        
+        pass
         
     def buzz(self, msg):
+        pass
         
     def on_message(self, client, userdata, msg):
+        print("message received " ,str(msg.payload.decode("utf-8")))
+        print("message topic=",msg.topic)
+        print("message qos=",msg.qos)
+        print("message retain flag=",msg.retain)
         route = msg.topic.split("/")
         print(route)
         node_data = msg.payload.decode("utf-8")
@@ -100,20 +124,22 @@ class SuperQuiz(tk.Frame):
 
         #todo - make this even vaguely object oriented
         if route[1] == 'player_registrar':
-            newPlayer(j['Player'])
+            self.newPlayer(j['Player'])
 
         elif route[1] == 'buzz':
-            buzz(j)
+            self.buzz(j)
 
     #MQTT events
     def on_connect(self, client, userdata, flags,rc):
         print("Connected with result code " + str(rc))
 
-        client.subscribe("SuperQuiz/#")        
+        client.subscribe("SuperQuiz/#")
+        print("Connected and waiting")
+        
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("1000x750")
-    root.title = "SuperQuiz"
+    root.title("SuperQuiz")
     mainWindow = SuperQuiz(root, bg=bgColor)
     mainWindow.pack_propagate(0)
     mainWindow.pack(side="top", fill="both", expand=True)
@@ -122,7 +148,7 @@ if __name__ == "__main__":
     client.on_connect = mainWindow.on_connect
     client.on_message = mainWindow.on_message
     client.connect("127.0.0.1", 1883, 60)
-    client.loop_forever()
+    client.loop_start()
 
     root.mainloop()
 
